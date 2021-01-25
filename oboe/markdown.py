18d7f07c70188518980d48df75d8c2f0f5b5a774
kmaasrud <km@aasrud.com>
2021-01-25 09:52:19 +0100

    Header formatter

--- oboe/markdown.py
+++ oboe/markdown.py
@@ -1,10 +1,17 @@
 import regex as re
+from oboe.utils import slug_case
+
 
 # Precompiled regexes
 whitespace_only_re = re.compile(r"^[ \t]+$", re.MULTILINE)
-code_block_re = re.compile(r"```(.*)$\n([\S\s]*?)\n```", re.MULTILINE)
+fenced_code_block_re = re.compile(r"```(.*)$\n([\S\s]*?)\n```", re.MULTILINE)
 footnote_refs_re = re.compile(r"\[\^([\p{L}\p{N}_.-]+?)\](?!:)")
 footnote_inline_re = re.compile(r"\^\[(.*?)\]")
+link_def_re = re.compile(
+    r"""^[ ]{0,3}\[(.+)\]:[ \t]*\n?[ \t]*<?(.+?)>?[ \t]*(?:\n?[ \t]*(?<=\s)['"(]([^\n]*)['")][ \t]*)?(?:\n+|\Z)""", re.M | re.U
+)
+header_re = re.compile(r"(^(.+)[ \t]*\n(=+|-+)[ \t]*\n+)|(^(\#{1,6})[ \t]+(.+?)[ \t]*(?<!\\)\#*\n+)", re.M)
+
 
 def convert(self, text):
     """Convert the given text."""
@@ -30,15 +37,19 @@ def convert(self, text):
     # contorted like /[ \t]*\n+/ .
     text = whitespace_only_re.sub("", text)
     
-    # Format code blocks
-    text = format_code_blocks(text)
+    # Format fenced code blocks
+    text = format_fenced_code_blocks(text)
 
     # Format footnotes
     text = format_footnotes(text)
 
-    # TODO: Continue here!
-    text = self._strip_link_definitions(text)
+    # Format link definitions (Not done yet)
+    text = format_link_definitions(text)
 
+    # Format all headers
+    text = format_headers(text)
+
+    # TODO: Continue here!
     text = self._run_block_gamut(text)
 
     if "footnotes" in self.extras:
@@ -91,8 +102,8 @@ def detab_line(line):
     return detab_line(chunk1 + chunk2)
 
 
-def format_code_blocks(text):
-    matches = code_block_re.finditer(text)
+def format_fenced_code_blocks(text):
+    matches = fenced_code_block_re.finditer(text)
     for match in matches:
         # Format as plaintext if no language specified
         lang = match.group(1) if match.group(1) else "plaintext"
@@ -109,18 +120,22 @@ def format_footnotes(text):
     for ref in footnote_refs:
         id = ref.group(1)
         if f"[^{id}]:" in text:
-            regex = re.compile(f"^ {{0,3}}\\[\\^{id}\\]: ?((?:\\s*.*\\n+)(?:^ {{4}}\\s*.*$\\n*)*)", re.MULTILINE)  
-            footnote = regex.search(text)
+            footnote_def_re = re.compile(f"^ {{0,3}}\\[\\^{id}\\]: ?((?:\\s*.*\\n+)(?:^ {{4}}\\s*.*$\\n*)*)", re.MULTILINE)
+            footnote = footnote_def_re.search(text)
+            # Remove footnote definitions
             text = text.replace(footnote.group(), "")
+            # Replace references with their HTML equivalent
             text = text.replace(ref.group(), f"<sup id=\"fn-{id}-ref\" class=\"footnote-ref\"><a href=\"#fn-{id}\" class=\"footnote-link\">[{footnote_number}]</a></sup>")
-            # Add footnotes as list element
+            # Add footnotes as li element
             footnote_html_section += f"<li id=\"fn-{id}\" class=\"footnote\">{footnote.group(1)}<a class=\"footnote-backref\" href=\"fn-{id}-ref\">↩︎</a></li>"
             footnote_number += 1
             
     # Then find inline footnotes
     footnote_refs = footnote_inline_re.finditer(text)
     for ref in footnote_refs:
+        # Replace references with their HTML equivalent
         text = text.replace(ref.group(), f"<sup id=\"fn-{footnote_number}-ref\" class=\"footnote-ref\"><a href=\"#fn-{footnote_number}\" class=\"footnote-link\">[{footnote_number}]</a></sup>")
+        # Add footnotes as li element
         footnote_html_section += f"<li id=\"fn-{footnote_number}\" class=\"footnote\">{ref.group(1)}<a class=\"footnote-backref\" href=\"fn-{footnote_number}-ref\">↩︎</a></li>"
 
     # Lastly add the footnote section at the end of the document
@@ -128,3 +143,25 @@ def format_footnotes(text):
         text += f"<section class=\"footnotes\"><hr>\n{footnote_html_section}\n</section>"
         
     return text
+
+
+def format_link_definitions(text):
+    # TODO: Not very important, will implement later. See regex `link_def_re`
+    return text
+
+
+def format_headers(text):
+    matches = header_re.finditer(text)
+    for match in matches:
+        if match.group(1) is not None and match.group(3) == "-":
+            return match.group(1)
+        elif match.group(1) is not None:
+            header_level = {"=": 1, "-": 2}[match.group(3)[0]]
+            header_text = match.group(2)
+        else:
+            n = len(match.group(5))
+            header_text = match.group(6)
+            
+        id = slug_case(header_text)
+        header_html = header_text
+        text.replace(match.group(), f"<h{n} id=\"{id}\">{header_html}</h{n}>")
