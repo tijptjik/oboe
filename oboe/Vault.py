eaf53e29e13d0b6fff97ba6a8783ba5039b23346
kmaasrud <km@aasrud.com>
2021-03-01 11:10:58 +0100

    Recursive dir search functional
    Finds all directories and notes properly, but the export paths are
    broken as of now...

--- oboe/Vault.py
+++ oboe/Vault.py
@@ -1,6 +1,7 @@
 import os
+import sys
 import regex as re
-from oboe.utils import slug_case, md_link, render_markdown, write
+from oboe.utils import slug_case, md_link, render_markdown, write, find_subdirs_recursively
 from oboe.Note import Note
 from oboe import LOG
 from oboe import GLOBAL
@@ -13,30 +14,32 @@ class Vault:
         self.extra_folders = extra_folders
 
         # If all folders are to be added
-        if extra_folders[0] == "all":
-            self.extra_folders = [] # remove the all statement, wich would have become a nonexisting folder.
-            for dirpath, _dirnames, _filenames in os.walk(vault_root):
-                dirpath = dirpath[len(self.vault_root):] #Slicing off the root folder part
-                self.extra_folders.append(dirpath)
+        if "all" in extra_folders:
+            LOG.debug("Adding notes from all subdirectories recursively.")
+            self.extra_folders = find_subdirs_recursively(vault_root)
 
-        self.notes = self._find_files(vault_root, self.extra_folders)
-        
+        self.notes = self._find_files()
+
         self._add_backlinks()
 
         if html_template:
             self.html_template_path = os.path.abspath(html_template)
-            with open(html_template, "r", encoding="utf8") as f:
-                self.html_template = f.read()
-        
-        
+            try:
+                with open(html_template, "r", encoding="utf8") as f:
+                    self.html_template = f.read()
+                LOG.debug(f"Using template: \"{os.path.abspath(self.html_template_path)}\"")
+            except FileNotFoundError:
+                LOG.error(f"Cannot find a template at path \"{self.html_template_path}\", aborting.")
+                sys.exit()
+
+        LOG.info(f"Created Vault object with root \"{os.path.abspath(vault_root)}\"")
 
 
     def _add_backlinks(self):
         for i, note in enumerate(self.notes):
             # Make temporary list of all notes except current note in loop
             others = [other for other in self.notes if other != note]
-            backlinks = self.notes[i].find_backlinks(others)
-            #print("BACKLINKS: " + str(backlinks))
+            backlinks = note.find_backlinks(others)
             if backlinks:
                 self.notes[i].backlink_html += "\n<div class=\"backlinks\" markdown=\"1\">\n"
                 for backlink in backlinks:
@@ -55,14 +58,14 @@ class Vault:
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
         for folder in self.extra_folders:
-            if not os.path.exists(out_dir + folder):
-                os.makedirs(out_dir + folder)
-                
+            if not os.path.exists(os.path.join(out_dir, folder)):
+                os.makedirs(os.path.join(out_dir, folder))
+
         if hasattr(self, "html_template"):
             stylesheets = re.findall('<link+.*rel="stylesheet"+.*href="(.+?)"', self.html_template)
             for stylesheet in stylesheets:
                 # Check if template contains reference to a stylesheet
-                stylesheet_abspath = os.path.join(os.path.dirname(self.html_template_path), stylesheet) 
+                stylesheet_abspath = os.path.join(os.path.dirname(self.html_template_path), stylesheet)
                 # Check if the referenced stylesheet is local, and copy it to out_dir if it is
                 if os.path.isfile(stylesheet_abspath):
                     GLOBAL.STYLESHEETS.append(stylesheet)
@@ -73,7 +76,7 @@ class Vault:
                     write(stylesheet_content, os.path.join(out_dir, stylesheet))
 
                     LOG.info("Copied local stylesheet into the output directory.")
-                
+
             # Use the supplied template on all notes
             for note in self.notes:
                 LOG.debug(f"Formatting {note.title} according to the supplied HTML template...")
@@ -103,16 +106,18 @@ class Vault:
                 LOG.debug(f"{note.title} written.")
 
 
-    def _find_files(self, vault_root, extra_folders):
-        md_files = []
+    def _find_files(self):
+        # Find all markdown-files in vault root.
+        md_files = self._find_files_in_dir(self.vault_root)
         # Find all markdown-files.
-        for folder in extra_folders:
-            md_files += self._find_md_files(vault_root + folder, is_extra_dir=True)
+        for folder in self.extra_folders:
+            md_files += self._find_files_in_dir(folder, is_extra_dir=True)
 
+        LOG.info(f"Found {len(md_files)} notes!")
         return md_files
 
 
-    def _find_md_files(self, folder, is_extra_dir=False):
+    def _find_files_in_dir(self, folder, is_extra_dir=False):
         md_files = []
         for md_file in os.listdir(folder):
             # Check if the element in 'folder' has the extension .md and is indeed a file
@@ -120,7 +125,7 @@ class Vault:
                 continue
 
             note = Note(os.path.join(folder, md_file), is_extra_dir=is_extra_dir, rel_dir=folder[len(self.vault_root)+1:]) #+1 removes the first / in the path, allowing os.path.join to work
-            
+
             # Filter tags
             if self.filter:
                 for tag in self.filter:
